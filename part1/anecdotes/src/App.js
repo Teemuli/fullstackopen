cc1a80a9a358203e23f71be713bbba107181b932
Teemuli <LitmTeemu@gmail.com>
2022-04-11 00:16:43 +0300

    Add exercises 1.12-1.14

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+function Header(props) {
+  return (
+    <h1>{props.name}</h1>
+  );
+}
+
+function Button(props){
+  return(
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  );
+}
+
+function Text(props) {
+  return (
+    <p>{props.name}</p>
+  );
+}
+
+function App() {
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ];
+   
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [mostvotes, setMostVotes] = useState(Math.floor(Math.random()*(anecdotes.length)));
+
+  const setToSelected = () => {
+    setSelected(Math.floor(Math.random()*(anecdotes.length)));
+  }
+
+  const addVote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+    const max = Math.max(...copy);
+    const index = copy.indexOf(max);
+    setMostVotes(index);
+  }
+
+  return (
+    <div>
+      <Header name={"Anecdote of the day"} />
+      <Text name={anecdotes[selected]} />
+      <Text name={"has "+votes[selected]+" votes"} />
+      <Button handleClick={() => setToSelected()} text="next anecdote" />
+      <Button handleClick={() => addVote()} text="vote" />
+      <Header name={"Anecdote with most votes"} />
+      <Text name={anecdotes[mostvotes]} />
+      <Text name={"has " + votes[mostvotes] + " votes"} />
+    </div>
+  )
+}
+
+export default App
