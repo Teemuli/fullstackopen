b528e1e2c29312366abfbe5be3408ffe0506d885
Teemuli <LitmTeemu@gmail.com>
2022-04-10 23:26:36 +0300

    Add exercises 1.6-1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,57 +1,73 @@
+import { useState } from 'react'
+
+function StatisticLine({name, value}){
+    return (
+      <tr>
+        <th align="left">{name}</th>
+        <th align="left">{value}</th>
+      </tr>
+    );
+}
+
+function Button(props){
+  return(
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  );
+}
+
 function Header(props) {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.name}</h1>
   );
 }
-function Content(props) {
-  
-  return(
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
-    </div>
-  );
-}
-function Total(props) {
-    return (
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    );
-}
 
-function Part(props){
-    return(
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    );
-}
-
-function App() {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+function Statistics ({good, neutral, bad}){
+  if(good === 0 && neutral === 0 && bad === 0){
+      return(<p>No feedback given</p>);
   }
+  return(
+    <table>
+      <tbody>
+      <StatisticLine name={"good"} value={good} />
+      <StatisticLine name={"neutral"} value={neutral} />
+      <StatisticLine name={"bad"} value={bad} />
+      <StatisticLine name={"all"} value={good+neutral+bad} />
+      <StatisticLine name={"avg"} value={(good-bad)/(good+neutral+bad)} />
+      <StatisticLine name={"positive"} value={((good)/(good+neutral+bad))*100+"%"} />
+      </tbody>
+    </table>
+  );
+}
+function App() {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const setGoodValue = newValue => {
+    setGood(newValue);
+  }
+
+  const setNeutralValue = newValue => {
+    setNeutral(newValue);
+  }
+
+  const setBadValue = newValue => {
+    setBad(newValue);
+  }
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <Header name={"give feedback"} />
+      <Button handleClick={() => setGoodValue(good + 1)} text="good" />
+      <Button handleClick={() => setNeutralValue(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBadValue(bad + 1)} text="bad" />
+      <Header name={"statistics"} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
